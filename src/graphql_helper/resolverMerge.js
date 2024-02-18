615c1d92d34a1018eedd1de45f0c55274caf2ad3
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-18 11:01:41 +0600

    some chnge

--- src/graphql_helper/resolverMerge.js
+++
@@ -1,7 +0,0 @@
-import { mergeResolvers } from "@graphql-tools/merge";
-import clientResolvers from "./resolvers/clientResolver";
-import todoResolvers from "./resolvers/todoResolvers";
-
-const resolvers = mergeResolvers([clientResolvers, todoResolvers]);
-
-export default resolvers;
--- src/graphql_helper/typeDefsMerge.js
+++
@@ -1,7 +0,0 @@
-import { mergeTypeDefs } from "@graphql-tools/merge";
-import clientType from "./typeDefs/clientTypes";
-import todoTypes from "./typeDefs/todoTypes";
-
-const typeDefs = mergeTypeDefs([clientType, todoTypes]);
-
-export default typeDefs;
