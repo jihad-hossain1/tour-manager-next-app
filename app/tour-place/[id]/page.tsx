7456e40a1b5acf29d1f04f3c1885fe88b91f9f06
Jihad Hossain <78225332+jihad-hossain1@users.noreply.github.com>
2024-04-29 10:43:05 +0600

    no data title added

--- app/tour-place/[id]/page.tsx
+++ app/tour-place/[id]/page.tsx
@@ -1,32 +1,36 @@
-import PageContainer from '@/components/ui/pageContainer';
-import { getContinent } from '@/service/query/continentQuery';
-import { Card, CardActionArea } from '@mui/material';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
+import NoData from "@/components/ui/NoData";
+import PageContainer from "@/components/ui/pageContainer";
+import { getContinent } from "@/service/query/continentQuery";
+import { Card } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Continentpage = async ({ params }) => {
     const { id } = params;
     const { data } = await getContinent(id);
-    const countries = data[0]
+    const countries = data[0];
 
     return (
         <PageContainer>
-            <div className='py-20 grid grid-cols-2 lg:grid-cols-4 gap-4'>
+          <div className="py-20">
+              {countries?.length > 0 ? (
+                  <div className=" grid grid-cols-2 lg:grid-cols-4 gap-4">
+                      {countries?.map((item, _i) => (
+                          <Link key={_i} href={`/tour-place/single-tour-place/${item?.id}`}>
+                              <Card className=" h-96 relative overflow-hidden">
+                                  {/* <Image width={300} height={300} alt="continent image" src={item?.photo} /> */}
+                                  <h4>{item?.name}</h4>
+                              </Card>
+                          </Link>
+            ))}
+                  </div>
+              ) : (
+                  <NoData title="No Tour Spot Found" LinkUrl="/tour-place" />
+              )}
+          </div>
+      </PageContainer>
+    );
+};
 
-                {
-                    countries?.map((item, _i) => (
-                        <Link key={_i} href={`/tour-place/single-tour-place/${item?.id}`}>
-                            <Card className=" h-96 relative overflow-hidden">
-                                {/* <Image width={300} height={300} alt="continent image" src={item?.photo} /> */}
-                                <h4>{item?.name}</h4>
-                            </Card>
-                        </Link>
-                    ))
-                }
-            </div>
-        </PageContainer>
-    )
-}
-
-export default Continentpage
+export default Continentpage;
---
+++ components/ui/NoData.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NoData = ({ title, LinkUrl }) => {
+    return (
+        <div className='flex justify-center items-center min-h-[70vh]'>
+            <div className='flex flex-col gap-2 items-center'>
+                <p>
+                    {title}
+                </p>
+                <Link href={LinkUrl} className='bg-blue-50 text-blue-500 shadow-sm hover:shadow px-3 py-1 rounded'>
+                    Go back
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default NoData
